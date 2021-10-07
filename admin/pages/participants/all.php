fbc454b7beddb20ea5c84563eef710a8cb9e9a61
ezechiel1 <ezechielkalengya@gmail.com>
2021-10-07 16:37:41 +0200

    Go Live

--- admin/pages/participants/all.php
+++ admin/pages/participants/all.php
@@ -93,8 +93,7 @@ $_PARTICIPATION_TYPE_TOKEN_ = Input::get('participationTypeToken', 'get');
 
                 <!-- <br><br> -->
                         </div>
-                        <!-- <div class="col-md-3"><button class="btn btn-sm btn-primary" style="height:33px !important; border-radius:0 !important;">Show details</button></div>
-                    </form> -->
+                    </div>
                 </div>
 
                 <div class="col-lg-12">
