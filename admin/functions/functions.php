93221388182ba155c04015b9ef59aee9f5e4d2b8
williamsclovis2 <clovismul@gmail.com>
2021-10-08 11:27:52 +0200

    profile

--- admin/functions/functions.php
+++ admin/functions/functions.php
@@ -16,8 +16,8 @@ function root($path) {
 
 // absulot path used in links
 function linkto($path) {
-	// echo "/apac/{$path}"; //Local
-    echo "/{$path}"; //Live
+	echo "/apac/{$path}"; //Local
+    // echo "/{$path}"; //Live
 }
 
 //Print Success Message Style
@@ -686,5 +686,5 @@ function html_cut($text, $max_length)
     return $result;
 }    
 
-// $INC_DIR = $_SERVER['DOCUMENT_ROOT'] . "/apac/admin/includes/"; //Local
-$INC_DIR = $_SERVER['DOCUMENT_ROOT'] . "/admin/includes/"; //Live
+$INC_DIR = $_SERVER['DOCUMENT_ROOT'] . "/apac/admin/includes/"; //Local
+// $INC_DIR = $_SERVER['DOCUMENT_ROOT'] . "/admin/includes/"; //Live
