4223e293f6f434d0e7a734010198538be430e1ef
williamsclovis2 <clovismul@gmail.com>
2021-10-12 12:31:40 +0200

    my changes

--- includes/nav-session.php
+++ includes/nav-session.php
@@ -4,19 +4,26 @@
                 <div class="container">
                     <div class="header_bottom_border">
                         <div class="row align-items-center">
-                            <div class="col-xl-2 col-lg-2">
+                            <div class="col-xl-2 col-lg-2" style="padding:0;">
                                 <div class="logo">
                                     <a href="<?php linkto('index'); ?>">
                                         <img src="<?php linkto('img/apac-web-logo.png'); ?>" class="img img-responsive" style="width: 150px;" alt="">
                                     </a>
                                 </div>
                             </div>
-                            <div class="col-xl-10 col-lg-10">
+                            <div class="col-xl-10 col-lg-10" style="padding:0;">
                                 <div class="main-menu  d-none d-xl-block">
                                     <nav>
                                     <ul id="navigation">
                                             <li><a href="<?php linkto('index'); ?>">Home</a></li>
+                                            <li><a href="<?php linkto('index'); ?>">About</a></li>
+                                            <li><a href="<?php linkto('index'); ?>">Programme</a></li>
+                                            <li><a href="<?php linkto('index'); ?>">Registration</a></li>
+                                            <li><a href="<?php linkto('index'); ?>">Call for proposals</a></li>
+                                            <li><a href="<?php linkto('index'); ?>">Media</a></li>
                                             <li><a href="#">Contact Us</a></li>
+                                            <li><a href="<?php linkto('index'); ?>">our partners</a></li>
+                                            <li><a href="<?php linkto('index'); ?>">Sponsorship</a></li>
                                             <?php if(isset($_SESSION['username'])) {?>
                                             <li><a href="<?php linkto('logout'); ?>">Logout</a></li>
                                             <?php } else { ?>
@@ -24,26 +31,26 @@
                                             <?php } ?>
                                             <li class="dropdown drop-language">
                                                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <?php
-                            // echo $_LangName;
-                            if($_LangName == 'English'):
-                                ?>
-                                                <img src="<?php linkto("img/En.png")?>">  English
-                            <?php
-                            elseif($_LangName == 'French'):
-                                ?>
-                                                <img src="<?php linkto("img/french.png")?>"> French
-                            <?php
-                            elseif($_LangName == 'Arabic'):
-                                ?>
-                                                <img src="<?php linkto("img/arabic.png")?>"> Arabic
-                            <?php
-                            elseif($_LangName == 'Portuguese'):
-                                ?>
-                                                <img src="<?php linkto("img/pt.png")?>"> Portuguese
-                            <?php
-                            endif;
-                                ?>
+                                                <?php
+                                                // echo $_LangName;
+                                                if($_LangName == 'English'):
+                                                    ?>
+                                                                    <img src="<?php linkto("img/En.png")?>">  English
+                                                <?php
+                                                elseif($_LangName == 'French'):
+                                                    ?>
+                                                                    <img src="<?php linkto("img/french.png")?>"> French
+                                                <?php
+                                                elseif($_LangName == 'Arabic'):
+                                                    ?>
+                                                                    <img src="<?php linkto("img/arabic.png")?>"> Arabic
+                                                <?php
+                                                elseif($_LangName == 'Portuguese'):
+                                                    ?>
+                                                                    <img src="<?php linkto("img/pt.png")?>"> Portuguese
+                                                <?php
+                                                endif;
+                                                    ?>
                                                 </button>
                                                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                                                     <a class="dropdown-item" onclick="multilangselect('eng-lang')"><img src="<?php linkto("img/En.png")?>"> English </a>
--- paymentmethod.php
+++ paymentmethod.php
@@ -24,7 +24,7 @@
                 <div class="row align-items-center justify-content-center">
                     <div class="col-lg-12">
                         <div class="slider_text slider_text_register">
-                            <h3 class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s" style="text-transform: none;">Choose Payment Method</h3>
+                            <h3 class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s" style="text-transform: none;">Please select a method of payment below. </h3>
                             <span class="separator-line wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s"></span>
                         </div>
                     </div>
@@ -36,36 +36,30 @@
     <div class="service_area about_event passes"  id="payment_area">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
-                    <div class="section_title mb-4 ">
-                    <p class="wow fadeInUp note" data-wow-duration="1s" data-wow-delay=".1s">Note:</p>
-                    <p class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s">Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="payment-meth wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s">
-                                <a class="btn btn-primary  DPO_link" href="#" style="width:100%;">Click to pay with one of  services listed  bellow </a>
-                                <div class="row img-card">
-                                    <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/visa.png"></a></div>
-                                    <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/MC.png"></a></div>
-                                    <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/AE.png"></a></div>
+                <div class="col-md-6">
+                    <div class="payment-meth wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s">
+                        <a class="btn btn-primary  DPO_link" href="#" style="width:100%;">Click to pay with one of  services listed  bellow </a>
+                        <div class="row img-card">
+                            <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/visa.png"></a></div>
+                            <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/MC.png"></a></div>
+                            <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/AE.png"></a></div>
 
-                                    <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/PP.png"></a></div>
-                                    <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/MP.png"></a></div>
-                                    <div class="col-md-2"></div>
-                                    <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/AT.png"></a></div>
-                                    <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/Tigo.jpg"></a></div>
-                                    <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/MM.png"></a></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="payment-meth wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s">
-                                <a class="btn btn-primary  DPO_link " href="#"><i class="fa fa-bank"></i> Pay using bank transfer</a>
-                                
-                            </div>
+                            <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/PP.png"></a></div>
+                            <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/MP.png"></a></div>
+                            <div class="col-md-2"></div>
+                            <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/AT.png"></a></div>
+                            <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/Tigo.jpg"></a></div>
+                            <div class="col-md-2"><a href="#"><img class="img img-responsive" src="<?=DN_IMG_CARDS?>/MM.png"></a></div>
                         </div>
                     </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="payment-meth wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s">
+                        <a class="btn btn-primary  DPO_link " href="#"><i class="fa fa-bank"></i> Pay using bank transfer</a>
+                        
+                    </div>
+                </div>
+            </div>
                 </div>
             </div>
         </div>
