d20c3202f88aac2d68717c06bfbf4ebdff5b83c4
williamsclovis2 <clovismul@gmail.com>
2021-10-17 09:29:54 +0200

    invoice pdf

--- admin/functions/functions.php
+++ admin/functions/functions.php
@@ -16,8 +16,8 @@ function root($path) {
 
 // absulot path used in links
 function linkto($path) {
-	// echo "/apac/{$path}"; //Local
-    echo "/{$path}"; //Live
+	echo "/apac/{$path}"; //Local
+    // echo "/{$path}"; //Live
 }
 
 //Print Success Message Style
