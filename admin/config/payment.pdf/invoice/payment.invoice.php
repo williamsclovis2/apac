e404c2bd38b0c7ebc7b3a06d5c0e6eacd930101a
williamsclovis2 <clovismul@gmail.com>
2021-10-17 11:31:20 +0200

    invoice compete

--- admin/config/payment.pdf/invoice/payment.invoice.php
+++ admin/config/payment.pdf/invoice/payment.invoice.php
@@ -8,20 +8,18 @@
 class myPDF extends FPDF
 {
   function header() {
-   $this->Image( VIEW_LOGO_APAC, 80,6,60);
+   $this->Image( VIEW_LOGO_APAC, 80,6,40);
   }
 
   function footer() {
     $this->SetY(-15);
     $this->SetFont('cambria','B',14);
     $this->Cell(0,15,'Page'.$this->PageNo().'/{nb}',0,0,'C');
-    $this->Ln();
   }
 
   function viewTable() {
 
 #Repport Divulgation
-    $this->Cell(190,5,'',0,3,'C');
     $this->SetFont('arial','B',11);
     $this->Cell(190,6,'',0,4,'C');
     $this->Cell(190,16,'',0,4,'C');
@@ -60,6 +58,29 @@ class myPDF extends FPDF
     $this->Cell(90,7,'$190 ',1,1,'L');
 
     $this->SetFont('arial','B',12);
+    $this->Cell(190,12,'Payment Options',0,1,'L');
+    $this->cell(190,0,'',1,1,'L');
+    $this->Ln();
+    $this->SetFont('arial','',10);
+    $this->Cell(190,11,'Payments can be made in either US$ of RWF. There are three payment options:',0,1,'L');
+    $this->Cell(190,11,'1.	By debit/credit card: Payment is accepted by VISA and/or MasterCard credit card and can be made directly ',0,1,'L');
+    $this->Cell(190,1,'    online through the registration system. ',0,1,'L');
+    $this->Ln();
+    $this->Ln();
+    $this->Cell(190,11,'    Upon completion of the registration process, your debit or credit card is charged immediately, and you ',0,1,'L');
+    $this->Cell(190,1,'    will receive a balance zero invoice (receipt) by e-mail. Please note that your monthly card statement will',0,1,'L');
+    $this->Cell(190,11,'    indicate payment made to the "Inaugural IUCN Africa Protected Areas Congress 2022".',0,1,'L');
+  
+    $this->Cell(190,11,'2.	By bank transfer:',0,1,'L');
+    $this->Cell(190,1,'- You must quote as a reference your Registration number, first and last names.',0,1,'L');
+    $this->Cell(190,11,'- Bank transfers are accepted in US$ or RWF only and the payee must cover all bank charges for the payment',0,1,'L');
+    $this->Cell(190,1,'- Bank fees are the sole responsibility of the registrant and should be paid in addition to the registration fees. ',0,1,'L');
+    
+    $this->Cell(190,11,'- All bank transfers must clearly state the name of the participant and the invoice number,',0,1,'L');
+    $this->Cell(190,1,' as unidentified bank transfers cannot be processed.',0,1,'L');
+   
+    $this->SetFont('arial','B',12);
+    $this->Cell(190,20,'',0,1,'L');
     $this->Cell(190,12,'BANK TRANSFER DETAILS:',0,1,'L');
     $this->SetFont('arial','',10);
     $this->Cell(190,1,'Bank Name: ECOBANK RWANDA PLC ',0,1,'L');
