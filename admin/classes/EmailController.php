864fa6681a5674516e4e3c1c3ace5294680246e0
ezechiel1 <ezechielkalengya@gmail.com>
2021-10-25 17:07:12 +0200

    Email Spacing

--- admin/classes/EmailController.php
+++ admin/classes/EmailController.php
@@ -628,7 +628,7 @@ class EmailController
   }
 
 
-  public static function emailSectionHeaderLayout(){
+  public static function emailSectionHeaderLayout(){ 
       $_HeaderLayout_ = "
       <!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
         <html xmlns='http://www.w3.org/1999/xhtml'>
