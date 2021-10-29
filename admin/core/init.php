5c1fa0362015e2a6853bd81b88d0b7b9b810ccef
ezechiel1 <ezechielkalengya@gmail.com>
2021-10-29 16:24:12 +0200

    local changes

--- admin/core/init.php
+++ admin/core/init.php
@@ -1,8 +1,8 @@
 <?php
 
 session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 // $_SESSION['user'] = 16;
 
 function def(){
@@ -62,49 +62,49 @@ $GLOBALS['config'] = array(
         'token_name' => 'token'
     ),
     'server' => array(
-         'name' => "http://{$_SERVER['HTTP_HOST']}/thefuture/apac",
-//         'name' => "http://{$_SERVER['HTTP_HOST']}", 
+        //  'name' => "http://{$_SERVER['HTTP_HOST']}/thefuture/apac",
+        'name' => "http://{$_SERVER['HTTP_HOST']}", 
     ),
     'root' => array(
-      'json_properties' => $_SERVER['DOCUMENT_ROOT']."/thefuture/apac/admin/config/json/properties.json", // Lccal
-//       'json_properties' => $_SERVER['DOCUMENT_ROOT']."/admin/config/json/properties.json" // Live
+    //   'json_properties' => $_SERVER['DOCUMENT_ROOT']."/thefuture/apac/admin/config/json/properties.json", // Lccal
+      'json_properties' => $_SERVER['DOCUMENT_ROOT']."/admin/config/json/properties.json" // Live
     ),
 
     'api' => array(
-         'payment_callback' => "http://197.243.23.101/thefuture/apac/pay/callback/",
-//       'payment_callback' => "http://{$_SERVER['HTTP_HOST']}/pay/callback/",
+        //  'payment_callback' => "http://197.243.23.101/thefuture/apac/pay/callback/",
+      'payment_callback' => "http://{$_SERVER['HTTP_HOST']}/pay/callback/",
     ),
 
     'url' => array(
         
-        'invitation_letter' => "http://{$_SERVER['HTTP_HOST']}/thefuture/apac/pdf/invitation/letter/",
-//         'invitation_letter' => "http://{$_SERVER['HTTP_HOST']}/pdf/invitation/letter/",
+        // 'invitation_letter' => "http://{$_SERVER['HTTP_HOST']}/thefuture/apac/pdf/invitation/letter/",
+        'invitation_letter' => "http://{$_SERVER['HTTP_HOST']}/pdf/invitation/letter/",
 
-        'invoice' => "http://{$_SERVER['HTTP_HOST']}/thefuture/apac/pdf/payment/invoice/",
-//         'invoice' => "http://{$_SERVER['HTTP_HOST']}/pdf/payment/invoice/",
+        // 'invoice' => "http://{$_SERVER['HTTP_HOST']}/thefuture/apac/pdf/payment/invoice/",
+        'invoice' => "http://{$_SERVER['HTTP_HOST']}/pdf/payment/invoice/",
         
-         'receipt' => "http://{$_SERVER['HTTP_HOST']}/thefuture/apac/pdf/payment/receipt/",
-//       'receipt' => "http://{$_SERVER['HTTP_HOST']}/pdf/payment/receipt/",
+        //  'receipt' => "http://{$_SERVER['HTTP_HOST']}/thefuture/apac/pdf/payment/receipt/",
+      'receipt' => "http://{$_SERVER['HTTP_HOST']}/pdf/payment/receipt/",
         
-		   'mail_smtp' => "http://{$_SERVER['HTTP_HOST']}/thefuture/apac/mail_smtp", // Local
-//		 'mail_smtp' => "http://{$_SERVER['HTTP_HOST']}/mail_smtp", // Live
+		//    'mail_smtp' => "http://{$_SERVER['HTTP_HOST']}/thefuture/apac/mail_smtp", // Local
+		 'mail_smtp' => "http://{$_SERVER['HTTP_HOST']}/mail_smtp", // Live
 
-		   'mail_smtp_noreply' => "http://{$_SERVER['HTTP_HOST']}/thefuture/apac/mail_smtp_noreply", // Local
-//		 'mail_smtp_noreply' => "http://{$_SERVER['HTTP_HOST']}/mail_smtp_noreply", // Live
+		//    'mail_smtp_noreply' => "http://{$_SERVER['HTTP_HOST']}/thefuture/apac/mail_smtp_noreply", // Local
+		 'mail_smtp_noreply' => "http://{$_SERVER['HTTP_HOST']}/mail_smtp_noreply", // Live
     ),
     'filepath' => array(
-		   'image' => $_SERVER['DOCUMENT_ROOT'].'/thefuture/apac/img/',  // Local
-//		 'image' => $_SERVER['DOCUMENT_ROOT'].'/img/',  //Live 
+		//    'image' => $_SERVER['DOCUMENT_ROOT'].'/thefuture/apac/img/',  // Local
+		 'image' => $_SERVER['DOCUMENT_ROOT'].'/img/',  //Live 
 	)
     
 );
 
- require_once $_SERVER['DOCUMENT_ROOT'] . '/thefuture/apac/admin/functions/functions.php'; // Local
-//  require_once $_SERVER['DOCUMENT_ROOT'] . '/admin/functions/functions.php'; // Live
+//  require_once $_SERVER['DOCUMENT_ROOT'] . '/thefuture/apac/admin/functions/functions.php'; // Local
+ require_once $_SERVER['DOCUMENT_ROOT'] . '/admin/functions/functions.php'; // Live
 
 spl_autoload_register(function($class) {
-   require_once $_SERVER['DOCUMENT_ROOT'] . '/thefuture/apac/admin/classes/' . $class . '.php'; // Local
-//   require_once $_SERVER['DOCUMENT_ROOT'] . '/admin/classes/' . $class . '.php'; // Live
+//    require_once $_SERVER['DOCUMENT_ROOT'] . '/thefuture/apac/admin/classes/' . $class . '.php'; // Local
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/admin/classes/' . $class . '.php'; // Live
 });
 
 /** Initialize Define */
@@ -139,5 +139,5 @@ $GLOBALS['_LangName'] = Functions::getLanguageName($_Lang);
 /** Dictionary */
 $GLOBALS['_Dictionary'] = new \Properties($_Lang);
 
-$INC_DIR = $_SERVER['DOCUMENT_ROOT'] . "/thefuture/apac/admin/includes/"; //Local
-// $INC_DIR = $_SERVER['DOCUMENT_ROOT'] . "/admin/includes/"; //Live
+// $INC_DIR = $_SERVER['DOCUMENT_ROOT'] . "/thefuture/apac/admin/includes/"; //Local
+$INC_DIR = $_SERVER['DOCUMENT_ROOT'] . "/admin/includes/"; //Live
